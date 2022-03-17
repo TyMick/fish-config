9ab10cd972ab3ca114c2ade9dcfa900f0239cc66
Ty Mick <ty.mick@faithlife.com>
2022-03-17 13:20:47 -0700

    `git pull` in config repos in update script

--- functions/update.fish
+++ functions/update.fish
@@ -1,4 +1,11 @@
 function update
+    set -l config_dirs (find ~/code/TyMick -maxdepth 1 -type l) ~/code/TyMick/vscode-settings
+    for dir in $config_dirs
+        cd $dir
+        git pull --quiet
+        cd -
+    end
+
     brew autoremove
     brew upgrade
     brew autoremove
