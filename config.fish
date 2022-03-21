96e890234e1969540b6f2f4c9614c725956c2e6e
Ty Mick <ty@tymick.me>
2022-03-20 18:20:30 -0700

    Install nvm.fish

--- config.fish
+++ config.fish
@@ -3,3 +3,6 @@ if status is-interactive
 end
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+# Use node version in .nvmrc if present in directory hierarchy
+nvm use &>/dev/null || true
