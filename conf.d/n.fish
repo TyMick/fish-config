ceb7ecaf5b1dee0e18a48e18af3d81eb0cf50516
Ty Mick <ty.mick@faithlife.com>
2022-07-02 16:14:51 -0700

    Remove auto node version choosing
    In favor of a tiny shim I wrote at `/usr/local/bin/node` instead:
    
    ```sh
    #!/bin/sh
    
    n run auto 2>/dev/null $@ || n run lts $@
    ```
    
    Quite proud of that.

--- conf.d/n.fish
+++
@@ -1,7 +0,0 @@
-function _choose_node_version --on-variable PWD
-    n --preserve auto 2>/dev/null ||
-    test (node -v) = v(n --lts) ||
-    n --preserve lts 2>/dev/null
-end
-
-_choose_node_version
