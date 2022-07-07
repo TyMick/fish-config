df2f27c3c9c5d161d4ecc7b0f7bc75bccb3d9fa1
Ty Mick <ty@tymick.me>
2022-07-07 16:56:37 -0700

    Add scoop shims

--- config.fish
+++ config.fish
@@ -6,7 +6,8 @@ set -l os (uname)
 if string match -q 'CYGWIN*' -- $os
     or string match -eiq Msys -- $os
     or string match -eiq mingw -- $os
-    fish_add_path --append --path /c/Users/ty.mick/scoop/apps/nodejs-lts/current \
+    fish_add_path --append --path /c/Users/ty.mick/scoop/shims \
+        /c/Users/ty.mick/scoop/apps/nodejs-lts/current \
         /c/Users/ty.mick/scoop/apps/yarn/current/bin
 end
 
