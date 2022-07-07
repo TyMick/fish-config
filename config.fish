d0ec41a76ea864cdc0ccb1cc35e8ab314fb55003
Ty Mick <ty@tymick.me>
2022-07-07 15:57:45 -0700

    Append to path on Windows

--- config.fish
+++ config.fish
@@ -2,4 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -l os (uname)
+if string match -q 'CYGWIN*' -- $os
+    or string match -eiq Msys -- $os
+    or string match -eiq mingw -- $os
+    fish_add_path --append --path /c/Users/ty.mick/scoop/apps/nodejs-lts/current \
+        /c/Users/ty.mick/scoop/apps/yarn/current/bin
+end
+
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
