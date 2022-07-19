c0ac57f68fa400138435853a30656f7a09cba311
Ty Mick <ty@tymick.me>
2022-07-18 17:24:54 -0700

    Adjust license notices

--- conf.d/git.fish
+++ conf.d/git.fish
@@ -1,6 +1,29 @@
-# Adapted from Oh My Zsh's git plugin (https://github.com/ohmyzsh/ohmyzsh/blob/0f2715b/plugins/git/git.plugin.zsh).
-# Original source copyright (c) 2009-2022 Robby Russell and contributors,
-# licensed under the MIT License (https://github.com/ohmyzsh/ohmyzsh/blob/0f2715b/LICENSE.txt).
+# Adapted from Oh My Zsh (https://github.com/ohmyzsh/ohmyzsh/blob/0f2715b/plugins/git/git.plugin.zsh)
+#
+# Original source license:
+#
+# # MIT License
+# #
+# # Copyright (c) 2009-2022 Robby Russell and contributors (https://github.com/ohmyzsh/ohmyzsh/contributors)
+# #
+# # Permission is hereby granted, free of charge, to any person obtaining a copy
+# # of this software and associated documentation files (the "Software"), to deal
+# # in the Software without restriction, including without limitation the rights
+# # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# # copies of the Software, and to permit persons to whom the Software is
+# # furnished to do so, subject to the following conditions:
+# #
+# # The above copyright notice and this permission notice shall be included in all
+# # copies or substantial portions of the Software.
+# #
+# # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# # SOFTWARE.
+
 
 # # Pretty log messages
 # function _git_log_prettily(){
