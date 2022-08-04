c28700d0e88e1693bb226089fb60c34a8f1ee177
Ty Mick <ty@tymick.me>
2022-08-04 14:32:01 -0700

    Port `grename`

--- conf.d/git.fish
+++ conf.d/git.fish
@@ -309,18 +309,18 @@ abbr -a gams git am --skip
 abbr -a gama git am --abort
 abbr -a gamscp git am --show-current-patch
 
-# function grename() {
-#   if [[ -z "$1" || -z "$2" ]]; then
-#     echo "Usage: $0 old_branch new_branch"
-#     return 1
-#   fi
+function grename
+    if test -z "$argv[1]" || test -z "$argv[2]" ]]
+        echo "Usage: $(status current-command) old_branch new_branch"
+        return 1
+    end
 
-#   # Rename branch locally
-#   git branch -m "$1" "$2"
-#   # Rename branch in origin remote
-#   if git push origin :"$1"; then
-#     git push --set-upstream origin "$2"
-#   fi
-# }
+    # Rename branch locally
+    git branch -m $argv[1] $argv[2]
+    # Rename branch in origin remote
+    if git push origin :$argv[1]; then
+        git push --set-upstream origin $argv[2]
+    end
+end
 
 # unset git_version
