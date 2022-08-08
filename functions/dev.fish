32432fcc154aa0507c786747b93171ab77c6839d
Ty Mick <ty@tymick.me>
2022-08-08 13:05:27 -0700

    Fix `dev` for new domain path components

--- functions/dev.fish
+++ functions/dev.fish
@@ -2,59 +2,71 @@
 # searches for the repo on GitHub, forks it if it isn't mine, clones it onto my
 # machine, and then opens it in VS Code.
 function dev
-    if string match -rq '^(?<account>[^/ ]+)/(?<repo>[^/ ]+)$' $argv[1]
-        set -l workspace_paths ~/code/$account/$repo.code-workspace ~/code/$account/$repo/$repo.code-workspace ~/code/$account/$repo/.vscode/$repo.code-workspace
-        for workspace_path in $workspace_paths
-            if test -e $workspace_path
-                open $workspace_path
-                return
-            end
-        end
+    set -f path (string split '/' $argv[1])
+    set -f repo $path[-1]
+    set -f account $path[-2]
+    set -f domain $path[-3]
 
-        if test -e ~/code/$account/$repo
-            # Parse symlinks so VS Code git integration doesn't get confused
-            code (readlink ~/code/$account/$repo || echo ~/code/$account/$repo)
+    # Try opening existing VS Code workspace
+    if test -n "$domain"
+        set -f workspace_paths ~/code/$domain/$account/$repo.code-workspace \
+            ~/code/$domain/$account/$repo/$repo.code-workspace \
+            ~/code/$domain/$account/$repo/.vscode/$repo.code-workspace
+    else if test -n "$account"
+        set -f workspace_paths ~/code/*/$account/$repo.code-workspace \
+            ~/code/*/$account/$repo/$repo.code-workspace \
+            ~/code/*/$account/$repo/.vscode/$repo.code-workspace
+    else
+        set -f workspace_paths ~/code/*/*/$repo.code-workspace \
+            ~/code/*/*/$repo/$repo.code-workspace \
+            ~/code/*/*/$repo/.vscode/$repo.code-workspace
+    end
+    for workspace_path in $workspace_paths
+        if test -e $workspace_path
+            open $workspace_path
             return
         end
+    end
 
-        mkdir -p ~/code/$account
-        if test $account = $github_username
-            if gh repo clone $account/$repo -- ~/code/$account/$repo
-                code ~/code/$account/$repo
-                return
-            end
-        else
-            cd ~/code/$account
-            if gh repo fork $account/$repo --clone=true
-                cd -
-                code ~/code/$account/$repo
-                return
-            end
-            cd -
-        end
-
-    else if string match -rq '^(?<repo>[^/ ]+)$' $argv[1]
-        set -l workspace_paths ~/code/*/$repo.code-workspace ~/code/*/$repo/$repo.code-workspace ~/code/*/$repo/.vscode/$repo.code-workspace
-        for workspace_path in $workspace_paths
-            if test -e $workspace_path
-                open $workspace_path
-                return
-            end
-        end
-
-        set -l dir_paths ~/code/*/$repo
-        for dir_path in $dir_paths
+    # Try opening existing directory
+    if test -n "$domain"
+        set -f dir_paths ~/code/$domain/$account/$repo
+    else if test -n "$account"
+        set -f dir_paths ~/code/*/$account/$repo
+    else
+        set -f dir_paths ~/code/*/*/$repo
+    end
+    for dir_path in $dir_paths
+        if test -e $dir_path
             # Parse symlinks so VS Code git integration doesn't get confused
             code (readlink $dir_path || echo $dir_path)
             return
         end
-
-        if gh repo clone $github_username/$repo -- ~/code/$github_username/$repo
-            code ~/code/$github_username/$repo
-            return
-        end
-
     end
+
+    # Try forking and cloning repo (or just cloning if the repo's mine)
+    if test -z "$domain"
+        set -f domain 'github.com'
+    end
+    if test -z "$account"
+        set -f account $github_username
+    end
+    mkdir -p ~/code/$domain/$account
+    for user in $my_github_users
+        if test "$domain/$account" = "$user"
+            if gh repo clone $domain/$account/$repo -- ~/code/$domain/$account/$repo
+                code ~/code/$domain/$account/$repo
+                return
+            end
+        end
+    end
+    cd ~/code/$domain/$account
+    if gh repo fork $domain/$account/$repo --clone=true
+        code $repo
+        cd -
+        return
+    end
+    cd -
 
     false
 end
