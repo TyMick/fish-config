d6baff5e87446e8becd56bc23820b0b89c60a573
Ty Mick <ty@tymick.me>
2022-09-14 12:44:21 -0700

    Fix `code` directory usage

--- functions/dev-link.fish
+++ functions/dev-link.fish
@@ -1,10 +1,10 @@
 # Symlinks the current directory into my dev folder hierarchy.
 function dev-link
     for remote in upstream origin
-        for regex in '^https?://(www\.)?github.com/(?<account>[^/]+)/(?<repo>[^/]+)\.git' '^https?://(www\.)?github.com/(?<account>[^/]+)/(?<repo>[^/]+)'
+        for regex in '^https?://(www\.)?(?<domain>[^/]+)/(?<account>[^/]+)/(?<repo>[^/]+)\.git' '^https?://(www\.)?(?<domain>[^/]+)/(?<account>[^/]+)/(?<repo>[^/]+)'
             if string match -rq $regex (git config --local remote.$remote.url)
-                mkdir -p ~/code/$account
-                ln -s "$PWD" ~/code/$account/$repo
+                mkdir -p ~/code/$domain/$account
+                ln -s "$PWD" ~/code/$domain/$account/$repo
                 return
             end
         end
