720ff57c820622411fe01ed22a18e6151a9a990f
Ty Mick <5317080+TyMick@users.noreply.github.com>
2023-01-19 11:57:59 -0800

    Restrict `rename_last_download` to files

--- functions/rename_last_download.fish
+++ functions/rename_last_download.fish
@@ -1,4 +1,4 @@
 function rename_last_download --argument-names new_name
-    set last_download (ls -t ~/Downloads | head -n1)
-    mv ~/Downloads/$last_download ~/Downloads/$new_name
+    set last_download_file (ls -pt ~/Downloads | grep --invert-match '/$' | head -n1)
+    mv ~/Downloads/$last_download_file ~/Downloads/$new_name
 end
