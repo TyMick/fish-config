eae49d1a8adc9133a868366b3ae2cae0b4bd43d7
Ty Mick <5317080+TyMick@users.noreply.github.com>
2023-03-15 10:28:46 -0700

    Source nodenv in WSL

--- config.fish
+++ config.fish
@@ -1,17 +1,5 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    if string match --quiet '*-microsoft-standard-WSL2' (uname --kernel-release)
+        nodenv init - fish | source
+    end
 end
-
-set -l os (uname)
-if string match -q 'CYGWIN*' -- $os
-    or string match -eiq Msys -- $os
-    or string match -eiq mingw -- $os
-    fish_add_path --append --path /c/Users/ty.mick/scoop/shims \
-        /c/Users/ty.mick/scoop/apps/nodejs-lts/current \
-        /c/Users/ty.mick/scoop/apps/yarn/current/bin
-end
-
-# pnpm
-set -gx PNPM_HOME "/Users/ty.mick/Library/pnpm"
-set -gx PATH "$PNPM_HOME" $PATH
-# pnpm end
