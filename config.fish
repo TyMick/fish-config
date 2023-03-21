41e24fa907452023dbe5661e350650565efdd3c0
Ty Mick <5317080+TyMick@users.noreply.github.com>
2023-03-21 10:30:07 -0700

    Fix uname call for Mac

--- config.fish
+++ config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
-    if string match --quiet '*-microsoft-standard-WSL2' (uname --kernel-release)
+    set -l kernel_release (uname -r)
+    if string match --quiet '*-microsoft-standard-WSL2' $kernel_release
         nodenv init - fish | source
     end
 end
