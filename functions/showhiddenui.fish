efe4007c515c2b8c2d16a5346c522b22064b9da2
Ty Mick <5317080+TyMick@users.noreply.github.com>
2023-05-20 10:37:22 -0700

    Parse `showhiddenui` args

--- functions/showhiddenui.fish
+++ functions/showhiddenui.fish
@@ -1,4 +1,7 @@
 function showhiddenui
+    argparse --min-args 1 --max-args 1 -- $argv
+    or return
+
     if test $argv = 'on'
         cmd.exe /c 'reg add HKCU\Software\Logos4 /v ShowHiddenUI /t REG_DWORD /d 1 /f'
         return
